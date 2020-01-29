a01091f28144f6d9fabf2205fea1d007d2e677c1
coexists <37199113+coexists@users.noreply.github.com>
2020-01-29 12:17:14 -0500

    Finish ICE

--- ice/w4/src/index.js
+++ ice/w4/src/index.js
@@ -1,8 +1,9 @@
 import Component from "./Component.js";
 import Task from "./Task.js";
+
 function runOnLoad()
 {
-    
+    console.log("New Change");
     let element = document.createElement("div");
     element.id = "Test";
     document.body.appendChild(element);
@@ -11,6 +12,16 @@ function runOnLoad()
     let element2 = document.createElement("div");
     element2.innerHTML = comp.render();
     console.log("Hello World truly");
+    let myTask = new Task(
+        {
+        content: "Finish ICE exercise",
+        done: false
+        }
+    )
+    var container = document.getElementById("taskList");
+    var newtask = document.createElement("div");
+    newtask.innerHTML = myTask.render();
+    container.appendChild(newtask);
+    console.log(myTask.render());
 }
-
 window.addEventListener("DOMContentLoaded", runOnLoad);
