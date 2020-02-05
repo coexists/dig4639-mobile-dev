2cc0ed46ce5471b8a34b0f6e38fcb75d6368cfbd
coexists <37199113+coexists@users.noreply.github.com>
2020-02-05 16:48:19 -0500

    lab 2

---
+++ labs/2/src/index.js
@@ -0,0 +1,16 @@
+
+DocViewer
+Pages
+// Import correct component
+let Card; // This creates a reference to the Card Class
+function runOnLoad()
+{
+    // Create a container for us
+    let element = document.createElement("div");
+    element.id = "container";
+    document.body.appendChild(element);
+    var newTask = new Card({content:"Sample value provided"});
+    element.appendChild(newTask.render());
+}
+window.addEventListener("DOMContentLoaded", runOnLoad);
+Annotations
