5eb58360130cf8998dfc9fe5bafd9f1414e3dfbc
coexists <37199113+coexists@users.noreply.github.com>
2020-02-07 16:10:24 -0500

    Lab 2

--- labs/2/src/index.js
+++ labs/2/src/index.js
@@ -1,16 +1,13 @@
-
-DocViewer
-Pages
-// Import correct component
-let Card; // This creates a reference to the Card Class
+import Card from "./components/Card/index.js";
 function runOnLoad()
 {
     // Create a container for us
     let element = document.createElement("div");
     element.id = "container";
     document.body.appendChild(element);
-    var newTask = new Card({content:"Sample value provided"});
-    element.appendChild(newTask.render());
+    var newCard = new Card({content:"Sample value provided"});
+    element.appendChild(newCard.render());
 }
+
+
 window.addEventListener("DOMContentLoaded", runOnLoad);
-Annotations
