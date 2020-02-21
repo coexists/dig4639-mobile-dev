e14f8db670bbd27cd0eb68f9a2f716804e9e9e40
coexists <37199113+coexists@users.noreply.github.com>
2020-02-21 15:49:01 -0500

    Lab 3 Yeahhhhhhhhhhhhh

--- labs/3/src/components/Card/index.js
+++ labs/3/src/components/Card/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./index.css";
 
-//example wk7 JSX: Stricter HTML
+
 class Card extends React.Component {
-    render(){
+    render() {
         return(
 
             <div className = "card">
