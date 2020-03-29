21cb5090f0b792ff476bb42a3ff662ba19b1617c
coexists <37199113+coexists@users.noreply.github.com>
2020-03-29 14:34:20 -0400

    lab 5

---
+++ labs/5/src/Components/CardList/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import WeatherCard from '../WeatherCard/index.js'
+
+class CardList extends React.Component {
+  
+  constructor (props) {
+    super(props)
+    this.state = {
+      periods: []
+    }}
+  componentDidMount() {
+    console.log("Component Mounted! :D")
+    fetch('https://api.weather.gov/gridpoints/MLB/25,69/forecast')
+      .then(res => res.json())
+      .then((result) => {
+        this.setState({
+          periods: result.properties.periods
+        })
+      })
+  }
+  render () {
+    return (
+      <>
+        {this.state.periods
+          .map((m) => <WeatherCard key={m.number}
+            name={m.name}
+            temperature={m.temperature}
+            temperatureUnit={m.temperatureUnit}
+            detailedForecast={m.detailedForecast}
+          />)}
+      </>
+    )}}
+    
+export default CardList
